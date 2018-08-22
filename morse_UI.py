2913d25f3a611990ef9936cfd2e09f49c255767a
Kasidech Chumkun <failkawae@gmail.com>
2018-08-23 01:55:20 +0700

    add morse diagram and fix some codes

--- morse_UI.py
+++ morse_UI.py
@@ -1,19 +1,25 @@
 #morse_UI.py
-import os 
 import tkinter
 from tkinter import *
+from PIL import Image, ImageTk
 import morse_translator
 
 #UI setup
 ui = tkinter.Tk()
-ui.title("Morse Translator Version 1.1")
-ui.geometry("400x150")
+ui.title("Morse Translator Version 1.1.1")
+ui.geometry("400x400")
 ui.resizable(width=False, height=False)
 
 title = Label(ui, text="Morse Translator",font=("Arial Bold",30))
 title.pack()
 
-inputarea =  Entry(ui)
+diagram_pic = Image.open("morse_diagram.png")
+resized = diagram_pic.resize((400, 250),Image.ANTIALIAS)
+image = ImageTk.PhotoImage(resized) 
+diagram = Label(ui, image = image)
+diagram.pack(side=BOTTOM)
+
+inputarea =  Entry(ui,justify="center")
 inputarea.pack(fill=X)
 
 outputarea =Label(ui, text="Type something...")
@@ -21,31 +27,31 @@ outputarea.pack()
 
 #function 
 def clicked(i : int):
-    if(i==1): # dot_clicked
+    if i == 1: # dot_clicked
          inputarea.insert(END,".")
-    elif(i==2): # dash_clicked
+    elif i == 2: # dash_clicked
         inputarea.insert(END,"-")
-    elif(i==3): # space_clicked
+    elif i == 3: # space_clicked
         inputarea.insert(END," ")
-    elif(i==4): # slash_clicked
+    elif i == 4: # slash_clicked
         inputarea.insert(END,"|") 
-    elif(i==5): # clear_clicked
+    elif i == 5: # clear_clicked
         inputarea.delete(0, END)
         outputarea.configure(text= "Type something...")
-    elif(i==6): # translate_clicked
+    elif i == 6: # translate_clicked
        data = inputarea.get()
-       if(data[0] == '.' or data[0] == '-'): # morse to text
+       if data[0] == '.' or data[0] == '-' : # morse to text
            output = morse_translator.toWord(data)
            outputarea.configure(text= output)
            morse_translator.toSound(data)
        else: # text to morse
             output = morse_translator.toCode(data)
             outputarea.configure(text= output)
-    elif(i==7): # copy_clicked
+    elif i == 7: # copy_clicked
         data = outputarea.cget("text")
         ui.clipboard_clear()
         ui.clipboard_append(data)
-    elif(i==8): # paste_clicked
+    elif i == 8: # paste_clicked
         data = ui.clipboard_get()
         inputarea.insert(END,data)
     
@@ -58,7 +64,7 @@ clear = Button(ui, text="Clear",command=lambda : clicked(5)).pack(padx=5,side=LE
 translate = Button(ui, text="Translate",command=lambda : clicked(6)).pack(padx=5,side=LEFT)
 copy = Button(ui, text="Copy",command=lambda : clicked(7)).pack(padx=5,side=LEFT)
 paste = Button(ui, text="Paste",command=lambda : clicked(8)).pack(padx=5,side=LEFT)
-                                                                
+
 #Run...
 if __name__ == '__main__':
      ui.mainloop()
